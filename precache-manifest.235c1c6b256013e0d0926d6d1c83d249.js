bd018c0a4ca8a2a77814799471b700564a329583
Rawan991 <rawan.alrajeh@gmail.com>
2020-02-26 16:05:23 +0300

    Updates

---
+++ precache-manifest.235c1c6b256013e0d0926d6d1c83d249.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f597aeb6e41a851693a727a1e0b3e173",
+    "url": "/.github.io/Project-2-React-List-App/index.html"
+  },
+  {
+    "revision": "ae1c05e086456d3e328b",
+    "url": "/.github.io/Project-2-React-List-App/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5c027fa0c0905ba0679e",
+    "url": "/.github.io/Project-2-React-List-App/static/css/main.05a37121.chunk.css"
+  },
+  {
+    "revision": "ae1c05e086456d3e328b",
+    "url": "/.github.io/Project-2-React-List-App/static/js/2.f723f7b1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/.github.io/Project-2-React-List-App/static/js/2.f723f7b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c027fa0c0905ba0679e",
+    "url": "/.github.io/Project-2-React-List-App/static/js/main.dd4e2e37.chunk.js"
+  },
+  {
+    "revision": "bead58e246dcdcc42d2e",
+    "url": "/.github.io/Project-2-React-List-App/static/js/runtime-main.5c3a5b96.js"
+  },
+  {
+    "revision": "13d99e709b8f51249c010e71f5317bd5",
+    "url": "/.github.io/Project-2-React-List-App/static/media/tdih-april23-HD.13d99e70.jpg"
+  }
+]);
